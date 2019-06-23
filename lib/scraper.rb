d87ebf1f6700454c5cfd87678ad2695007de4377
Blake Rose <bdecktor@gmail.com>
2019-06-23 01:25:46 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -22,7 +22,7 @@ class Scraper
   self.get_page.css(".post")
 end
 
-def print_courses
+def make_courses
   self.get_courses.each do |post|
     course = Course.new
     course.title = post.css("h2").text
